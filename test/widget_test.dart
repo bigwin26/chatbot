326b301e96e583a06718583bdae15e8bc2a49ff7
bigwin26 <kdsgood12@daum.net>
2025-02-19 17:42:24 +0900

    chatbot ui적용

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,16 +4,15 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:chatbot/main.dart';
+import 'package:chatbot/presentation/screens/chat.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ChatBot());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
